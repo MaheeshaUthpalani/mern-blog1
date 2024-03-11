ebfb9a2bbbebd1453d0e57a2d0fcd082859fd599
Maheesha <maheeu2000@gmail.com>
2024-03-11 23:56:49 +0530

    add a middleware and a function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,20 @@
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
-export const contactus = async (req, res) => {
+export const contactus = async (req, res, next) => {
     const { name, email, contactNo, message } = req.body;
 
-    if (!name || !email || !contactNo || !message || name === '' || email === '' || contactNo === '' || message === '') {
-        return res.status(400).json({ message: 'All fields are required' });
+    if (
+        !name || 
+        !email || 
+        !contactNo || 
+        !message || 
+        name === '' || 
+        email === '' || 
+        contactNo === '' || 
+        message === ''
+    ) {
+      next(errorHandler(400, 'All fields are required'));  
     }
 
     const newUser = new User ({
@@ -14,6 +24,12 @@ export const contactus = async (req, res) => {
         message,
     });
 
-    await newUser.save();
-    res.json( 'Inquiary successful' );
+    try {
+        await newUser.save();
+        res.json( 'Inquiary successful' );
+    }catch (error){
+        next(error);
+    }
+    
+    
 };
